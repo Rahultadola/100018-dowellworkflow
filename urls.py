7080e10f6481c6032fcb04f9c190b98546948607
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-10-05 13:00:46 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import create_document_type, getDocumentTypeObject, EditorView, DocumentCreatedListView, DocumentWorkFlowAddView, DocumentExecutionListView, DocumentVerificationView
+from .views import editor, workflow_delete_view, workflow_update_view, create_document_type, getDocumentTypeObject, WorkFlowListView, DocumentCreatedListView, DocumentWorkFlowAddView, DocumentExecutionListView, DocumentVerificationView
 
 
 app_name = 'workflow'
 
 urlpatterns = [
-    path('editor/', login_required(EditorView.as_view()), name='editor'),
+    path('editor/', login_required(editor)),
+    path('workflow-list/', login_required(WorkFlowListView.as_view()), name="work-flow-list"),
+    path('wf_api/delete-wf/', login_required(workflow_delete_view)),
+    path('wf_api/update-wf/', login_required(workflow_update_view)),
     path('created-document-list/', login_required(DocumentCreatedListView.as_view()), name='created-document-list'),
     path('create-document-type/', login_required(create_document_type), name="create-document-type"),
     path('detail-document-type/<int:id>', login_required(getDocumentTypeObject), name="detail-document-type"),
@@ -14,5 +17,3 @@ urlpatterns = [
     path('documents-in-workflow/', login_required(DocumentExecutionListView.as_view()), name="documents-in-workflow"),
     path('verify-document/<int:id>', login_required(DocumentVerificationView.as_view()), name="verify-document")
 ]
-
-
